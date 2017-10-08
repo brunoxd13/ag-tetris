3c5beca8c281a35ba5b9a77afd70be9e6acbc98c
Bruno Russi <brunoxd13@gmail.com>
2017-10-07 22:08:41 -0300

    correcao de bug

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import algoritmoGenetico as ag
 
 def main():
 
-    numInd = 10
+    numInd = 2
     geracao = ag.Geracao(numInd)
 
     vel_jogo = 500
@@ -12,25 +12,15 @@ def main():
         print (' - - - - Geração atual: {} - - - - ' .format(j+1))
         print (' \n')
         
-        for i in range(10):
+        for i in range(numInd):
             gameState = jogo.jogar(geracao.individuos[i], vel_jogo, scoreMax = 200000, jogoRapido = False)
-            
-            print(gameState)
-
             geracao.individuos[i].fitness(gameState)
-            geracao.selecao(1)
-
-            print("melhor individuo da geração!!!")
 
         geracao.selecao(3)
         geracao.reproduzir(10)
 
-    geracao.selecao(1)
     print("melhor individuo!!!")
-
     gameState = jogo.jogar(geracao.individuos[0], vel_jogo, 1)    
-
     return(geracao)
-
 
 gen = main()
